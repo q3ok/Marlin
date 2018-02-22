a1cc312efc2c4a9b8cc31d03f422cb39b0cd6f72
q3ok <kkuczok@gmail.com>
2018-02-22 17:44:50 +0100

    cleanup #1

--- Marlin/pins_RAMPS.h
+++ Marlin/pins_RAMPS.h
@@ -91,12 +91,12 @@
 #define X_STEP_PIN         54
 #define X_DIR_PIN          55
 #define X_ENABLE_PIN       38
-#define X_CS_PIN           53
+#define X_CS_PIN           40
 
 #define Y_STEP_PIN         60
 #define Y_DIR_PIN          61
 #define Y_ENABLE_PIN       56
-#define Y_CS_PIN           44
+#define Y_CS_PIN           42
 
 #define Z_STEP_PIN         46
 #define Z_DIR_PIN          48
@@ -106,7 +106,7 @@
 #define E0_STEP_PIN        26
 #define E0_DIR_PIN         28
 #define E0_ENABLE_PIN      24
-#define E0_CS_PIN          42
+#define E0_CS_PIN          65
 
 #define E1_STEP_PIN        36
 #define E1_DIR_PIN         34
